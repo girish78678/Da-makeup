7692647b83ade917bac603ac53970d33411e4990
girish sharma <girish@sozeno.com>
2023-01-11 19:05:15 +0530

    update da-makeup site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import * as React from "react"
+import "../styles/normalize.css"
+import "../styles/style.css"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>Hello world!
+  </div>
 }
